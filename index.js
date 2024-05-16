fcaf41afb7d9e02831812945da065797f3c27b99
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-16 11:06:00 -0400

    pass test 1

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Add your code here
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+// });
+
+const formData = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+function submitData() {
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+}
