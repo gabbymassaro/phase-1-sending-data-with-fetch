ea5f0cdfcb9a8bdf346e4cacee559d3ba54140cc
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-16 21:12:17 -0400

    create catch and append msg to dom

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ function submitData(name, email) {
     },
     body: JSON.stringify({name: name, email: email}),
   };
+
   return fetch("http://localhost:3000/users", configurationObject)
   .then(function (response) {
     return response.json();
   })
   .then(function (object) {
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.")
-    console.log(object.id);
     const body = document.querySelector('body');
     const div = document.createElement('div')
     div.innerHTML = object.id
     body.appendChild(div)
   })
+  .catch(function (object) {
+    const body = document.querySelector('body');
+    const div = document.createElement('div')
+    div.innerHTML = object
+    body.appendChild(div)  })
 }
